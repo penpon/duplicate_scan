9436a550b0c7c86f305188ea9f9643605456ddb2
penpon <haysato.work@gmail.com>
2025-11-26 16:58:26 +0900

    update

--- scripts/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run tests if they exist, otherwise skip gracefully
-if [ -d "tests" ] && [ "$(find tests -name "*.py" -type f | wc -l)" -gt 0 ]; then
-    uv run pytest -n auto tests/ -v
-else
-    echo "No tests found"
-fi
